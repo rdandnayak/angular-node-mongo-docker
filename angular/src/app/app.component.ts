b6d14add61ed264530d0fda204afdbc085e3ab60
Rohit Dandnayak <rohitdandnayak@Rohits-Air.domain.name>
2021-04-08 15:49:22 +0530

    updated app

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { finalize, repeatWhen, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,24 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'angular';
   todo$: Observable<any>;
-
+  private reload = new Subject();
   constructor(private http: HttpClient) {}
   ngOnInit() {
-    this.todo$ = this.http.get('/todo');
+    this.todo$ = this.http
+      .get('/todo')
+      .pipe(repeatWhen(() => this.reload.asObservable()));
+  }
+
+  async addTodo($event) {
+    await this.http
+      .post('/todo', {
+        body: {
+          item: $event.target.value,
+        },
+      })
+      .pipe(finalize(() => this.reload.next()))
+      .toPromise();
+
+    $event.target.value = '';
   }
 }
